63c2802b8297bf269f2d45b35a0f6e2d1af9df88
begbie86 <simon.r.staub@gmail.com>
2016-06-20 20:14:47 +0200

    First

---
+++ FerienClient/FerienClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Zusätzliche Namespaces
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Discovery;
+
+namespace FerienClient
+{
+    [ServiceContract]
+    public interface IFlug
+    {
+        [OperationContract]
+        string getFlug(DateTime tDatetime, string tStartStadt, string tZielStadt);
+    }
+
+    [ServiceContract]
+    public interface IHotel
+    {
+        [OperationContract]
+        string getHotel(DateTime tDate, string tDestination);
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dynamicFlugEndpoint = new DynamicEndpoint(ContractDescription.GetContract(typeof(IFlug)), new WSHttpBinding(SecurityMode.None));
+            var dynamicHotelEndpoint = new DynamicEndpoint(ContractDescription.GetContract(typeof(IHotel)), new WSHttpBinding(SecurityMode.None));
+
+            var proxyFlug = new ChannelFactory<IFlug>(dynamicFlugEndpoint).CreateChannel();
+            var proxyHotel = new ChannelFactory<IHotel>(dynamicHotelEndpoint).CreateChannel();
+
+            Console.WriteLine("Wohin wollen Sie wann gehen?");
+            DateTime dt = new DateTime(2016, 08, 31);
+
+            Console.WriteLine(proxyFlug.getFlug(dt,"Bern","Berlin"));
+            Console.WriteLine(proxyHotel.getHotel(dt, "Berlin"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HotelServer/HotelServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Zusätzliche Namespaces
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Discovery;
+
+namespace HotelServer
+{
+    [ServiceContract]
+    public interface IHotel
+    {
+        [OperationContract]
+        string getHotel(DateTime tDate, string tDestination);
+    }
+
+    class Hotel:IHotel
+    {
+        public string getHotel(DateTime tDate, string tDestination)
+        {
+            return "31.08.2018,Berlin,Sheraton,650";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ServiceHostHotels = new ServiceHost(typeof(Hotel));
+            ServiceHostHotels.Open();
+
+            Console.WriteLine("HotelService Running...");
+            Console.ReadLine();
+        }
+    }
+}
